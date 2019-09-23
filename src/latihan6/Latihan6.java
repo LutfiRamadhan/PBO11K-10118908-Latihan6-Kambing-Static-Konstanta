035479dc285831557506b7742bbe9fa03d42a3cc
Lutfi Ramadhan <lutfiramadan@gmail.com>
2019-09-23 22:41:27 +0700

    First Commit

---
+++ src/latihan6/Latihan6.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan6;
+
+/**
+ *
+ * @author lutfi
+ * NAMA  : LUTFI RAMADHAN
+ * NIM   : 10118908
+ * KELAS : IF 11 Karyawan
+ */
+
+public class Latihan6 {
+
+    /**
+     */
+    public static final String NAMA_KAMBING = "MIDUN";
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mamalia.jumlahKambing = 485000;
+        System.out.println(NAMA_KAMBING+" memiliki kambing sebanyak "+Mamalia.jumlahKambing);
+    }
+  
+}
---
+++ src/latihan6/Mamalia.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan6;
+
+/**
+ *
+ * @author lutfi
+ * NAMA  : LUTFI RAMADHAN
+ * NIM   : 10118908
+ * KELAS : IF 11 Karyawan
+ */
+public class Mamalia {
+    public static int jumlahKambing;
+}
